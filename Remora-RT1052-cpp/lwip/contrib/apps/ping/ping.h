0fc452dcf25ee2d0803b3f04dead87d0a96ef224
scott <scottalford75@gmail.com>
2023-06-21 12:56:05 +1000

    Repo setup

---
+++ Remora-RT1052-cpp/lwip/contrib/apps/ping/ping.h
@@ -0,0 +1,27 @@
+#ifndef LWIP_PING_H
+#define LWIP_PING_H
+
+#include "lwip/ip_addr.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * PING_USE_SOCKETS: Set to 1 to use sockets, otherwise the raw api is used
+ */
+#ifndef PING_USE_SOCKETS
+#define PING_USE_SOCKETS    LWIP_SOCKET
+#endif
+
+void ping_init(const ip_addr_t* ping_addr);
+
+#if !PING_USE_SOCKETS
+void ping_send_now(void);
+#endif /* !PING_USE_SOCKETS */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* LWIP_PING_H */
